6873f575339006d9b527e3bcf773843f4a708e48
Ivan <ivan.skif41@gmail.com>
2024-03-19 09:53:33 +0100

    Fix an issue with opacity when user tap on a button.

--- ChallenegeSeven/ContentView.swift
+++ ChallenegeSeven/ContentView.swift
@@ -16,18 +16,23 @@ struct RectPreference: PreferenceKey {
 }
 
 struct MyButtonStyle: ButtonStyle {
+    @Binding var opacity: Double
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
             .foregroundStyle(.white)
             .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.blue))
             .opacity(configuration.isPressed ? 0.5 : 1)
+            .onChange(of: configuration.isPressed) { _, newValue in
+                opacity = newValue == true ? 0.5 : 1
+            }
     }
 }
 
 struct ContentView: View {
     @Namespace private var animation
     @State private var isZoomed = false
+    @State private var size: CGSize = .zero
     
     var frame: CGSize {
         !isZoomed ? size : CGSize(width: 250, height: 400)
@@ -37,18 +42,19 @@ struct ContentView: View {
         !isZoomed ? .bottomTrailing : .center
     }
     
-    @State private var size: CGSize = .zero
-    
     private let cornerRadius: CGFloat = 10
     
     private let buttonText: String = "1"
     private let button: String = "2"
     private let plate: String = "3"
+    
+    @State private var plateOpacity: Double = 1
+    
     var body: some View {
         ZStack(alignment: .top) {
             ZStack(alignment: .topLeading) {
                     RoundedRectangle(cornerRadius: cornerRadius)
-                    .foregroundStyle(.blue)
+                    .foregroundStyle(.blue.opacity(plateOpacity))
                     .frame(width: frame.width, height: frame.height)
                 if isZoomed {
                     Button{
@@ -57,7 +63,7 @@ struct ContentView: View {
                         }
                     } label: {
                         Label("Back", systemImage: "arrowshape.backward.fill")
-                            .foregroundColor(.white)
+                            .foregroundStyle(.white)
                             .fixedSize(horizontal: true, vertical: true)
                             .padding()
                             .matchedGeometryEffect(id: buttonText, in: animation)
@@ -82,11 +88,10 @@ struct ContentView: View {
                             .preference(key: RectPreference.self, value: proxy.size)
                     }
                 }
-                .buttonStyle(MyButtonStyle())
+                .buttonStyle(MyButtonStyle(opacity: $plateOpacity))
                 .matchedGeometryEffect(id: button, in: animation)
-                .onPreferenceChange(RectPreference.self, perform: { value in
-                    print("Preference changed: \(value)")
-                    size = value
+                .onPreferenceChange(RectPreference.self, perform: {
+                    size = $0
                 })
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: alignment)
             }
